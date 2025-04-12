2ec1f1be7a9fb605dd00723652ff44fe69812109
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2025-04-12 01:45:21 -0300

    feat - Setting up: - Swagger documentation with ScalarUI - Validations, serialization and scheme transformation with zod

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,54 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import scalarUI from '@scalar/fastify-api-reference'
 import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
 
+server.setSerializerCompiler(serializerCompiler)
+server.setValidatorCompiler(validatorCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      errors: error.validation,
+    })
+  }
+
+  console.error(error)
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
+
 server.register(fastifyCors, {
   origin: '*',
+})
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Brev.ly API Docs',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(scalarUI, {
+  routePrefix: '/docs',
+  configuration: {
+    theme: 'purple',
+    layout: 'modern',
+  },
 })
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
