ec3d35ae5d6e8a26523b728b93a253c845ac5e4b
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2025-04-13 14:25:10 -0300

    feat - Setting up on R2 Bucket Cloudflare: - Creating necessary env variables:   - CLOUDFLARE_ACCOUNT_ID   - CLOUDFLARE_ACCESS_KEY_ID   - CLOUDFLARE_SECRET_ACCESS_KEY   - CLOUDFLARE_BUCKET   - CLOUDFLARE_PUBLIC_URL - Adding password env variable to connect redis db - Adding on dev script to read .env file

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,12 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   POSTGRES_URL: z.string().url().startsWith('postgresql://'),
   REDIS_URL: z.string().url().startsWith('redis://'),
+  REDIS_PASSWORD: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+  CLOUDFLARE_BUCKET: z.string(),
+  CLOUDFLARE_PUBLIC_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/infra/db/redis.ts
+++ src/infra/db/redis.ts
@@ -3,6 +3,7 @@ import { createClient } from 'redis'
 
 export const redis = createClient({
   url: env.REDIS_URL,
+  password: env.REDIS_PASSWORD,
 })
 
 redis.connect()
