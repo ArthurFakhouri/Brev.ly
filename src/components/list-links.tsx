9816c039d6509ca57f0afcbcef6e74840e7657de
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2025-04-18 22:17:04 -0300

    feat - developing my links card: - Applying export from button, with disabled, loading while generate file. - Applying animations on list when a link is inserted or removed - Applying logical to delete link and logical to copy link url - Applying logical when clicking on link - Removing scroll-area component - Applying scroll style

--- src/components/list-links.tsx
+++ src/components/list-links.tsx
@@ -1,11 +1,9 @@
-import { Copy, Trash } from "@phosphor-icons/react"
-import { Button } from "./Button"
-
-type LinkProps = {
-  linkOriginal: string
-  linkShortened: string
-  numberOfAccess: number
-}
+import { useListLinks } from "@/hooks/useListLinks"
+import type { LinkProps } from "@/types/links"
+import { AnimatePresence, motion } from 'motion/react'
+import { CopyButton } from "./copy-button"
+import { LinkImage } from "./link-image"
+import { RemoveLinkButton } from "./remove-link-button"
 
 type ListLinksProps = {
   listLinks: LinkProps[]
@@ -14,31 +12,74 @@ type ListLinksProps = {
 export function ListLinks({
   listLinks,
 }: ListLinksProps) {
+
+  const { handleOpenLink, handleRemoveLink } = useListLinks()
+
   return (
-    <ul className="flex flex-col w-full [&>li+li]:border-t-gray-200">
-      {listLinks.map(link => {
-        return (
-          <li key={link.linkShortened} className="flex gap-4 border-t border-t-transparent w-full items-center py-3 md:gap-5 md:py-4">
-            <div className="flex min-w-[50px] flex-1 flex-col gap-1">
-              <strong className="text-blue-base text-sm font-semibold line-clamp-1 leading-[1.125rem]">
-                {link.linkShortened}
-              </strong>
-              <span className="text-gray-500 text-xs line-clamp-1">
-                {link.linkOriginal}
+    <motion.ul
+      className="flex flex-col items-center w-full [&>li+li]:border-t-gray-200"
+      layout
+      transition={{
+        layout: {
+          duration: .3,
+          ease: [.33, -.25, .33, 1],
+        }
+      }}
+      initial={{ height: 0 }}
+      animate={{
+        height: 'auto',
+      }}
+    >
+      <AnimatePresence>
+        {listLinks.map(link => {
+          return (
+            <motion.li
+              key={link.urlShortened}
+              className="flex gap-4 border-t border-t-transparent max-h-[66px] items-center w-full py-3 md:gap-5 md:py-4 md:max-h-[74px]"
+              layout
+              transition={{
+                layout: {
+                  duration: .3,
+                  ease: [.33, -.25, .33, 1],
+                }
+              }}
+              initial={{ transform: 'translateY(-50%) scale(0)', height: 0 }}
+              animate={{
+                transform: 'translateY(0) scale(1)',
+                height: '100%',
+                transition: {
+                  duration: .3,
+                  ease: [.33, -.25, .33, 1],
+                }
+              }}
+              exit={{ transform: 'translateY(-50%) scale(0)', height: 0 }}
+            >
+              <div className="flex min-w-[0px] flex-1  flex-col gap-1">
+                <button
+                  type="button"
+                  onClick={() => handleOpenLink(link.urlShortened)}
+                  className="text-blue-base text-left text-sm font-semibold line-clamp-1 leading-[1.125rem] hover:cursor-pointer hover:text-blue-dark"
+                >
+                  {link.urlShortened}
+                </button>
+                <span className="flex-1 text-gray-500 text-xs line-clamp-1 flex gap-1">
+                  <LinkImage link={link.urlOriginal} />
+                  {link.urlOriginal}
+                </span>
+              </div>
+              <span className="text-gray-500 text-xs text-center">
+                {link.numberOfAccess === 0 && 'Nenhum acesso'}
+                {link.numberOfAccess === 1 && '01 acesso'}
+                {link.numberOfAccess > 1 && `${link.numberOfAccess.toString().padStart(2, '0')} acessos`}
               </span>
-            </div>
-            <span className="text-gray-500 text-xs">{link.numberOfAccess.toString().padStart(2, '0')} acessos</span>
-            <div className="flex gap-1">
-              <Button variant="secondary" icon>
-                <Copy size={16} className="text-gray-600" />
-              </Button>
-              <Button variant="secondary" icon>
-                <Trash size={16} className="text-gray-600" />
-              </Button>
-            </div>
-          </li>
-        )
-      })}
-    </ul>
+              <div className="flex gap-1">
+                <CopyButton contentToCopy={link.urlShortened} />
+                <RemoveLinkButton removeFn={async () => { await handleRemoveLink(link.urlShortened) }} />
+              </div>
+            </motion.li>
+          )
+        })}
+      </AnimatePresence>
+    </motion.ul>
   )
 }
--- src/components/my-links.tsx
+++ src/components/my-links.tsx
@@ -1,34 +1,41 @@
-import { DownloadSimple } from "@phosphor-icons/react";
-import { Button } from "./Button";
-import { Box } from "./box";
+import type { LinkProps } from "@/types/links";
+import { motion } from "motion/react";
+import { DownloadCSVButton } from "./download-csv-button";
 import { EmptyListLinks } from "./empty-list-links";
 import { ListLinks } from "./list-links";
-
-type LinkProps = {
-  linkOriginal: string
-  linkShortened: string
-  numberOfAccess: number
-}
+import { Box } from "./ui/box";
 
 type MyLinksProps = {
   listLinks: LinkProps[]
+  isLoading: boolean
 }
 
 export function MyLinks({
   listLinks,
+  isLoading,
 }: MyLinksProps) {
+
   return (
-    <Box className="flex-1 max-w-[380px] w-full gap-4 md:gap-5 md:max-w-full">
+    <Box className="relative flex-1 max-w-[380px] overflow-hidden w-full gap-4 md:gap-5 md:max-w-full">
+      {isLoading && <motion.div
+        className="absolute left-0 -translate-x-[150%] top-0 w-[30%] bg-blue-base h-0.5"
+        animate={{ transform: 'translateX(500%)' }}
+        transition={{
+          duration: 1,
+          repeat: Infinity,
+          ease: [.89, .18, .7, .73]
+        }}
+      />
+      }
       <div className="w-full flex items-center justify-between">
         <h2 className="text-gray-600 font-bold text-lg leading-6">Meus links</h2>
-        <Button variant="secondary">
-          <DownloadSimple size={16} className="text-gray-600" />
-          Baixar CSV
-        </Button>
+        <DownloadCSVButton />
       </div>
-      <div className="border-t border-t-gray-200">
-        {listLinks.length === 0 && <EmptyListLinks />}
+      <div
+        className="border-t w-full pr-2 max-h-[348px] overflow-x-hidden overflow-y-auto scroll border-t-gray-200 md:max-h-[500px] [&>div]:pr-4"
+      >
         {listLinks.length !== 0 && <ListLinks listLinks={listLinks} />}
+        {listLinks.length === 0 && <EmptyListLinks />}
       </div>
     </Box>
   )
